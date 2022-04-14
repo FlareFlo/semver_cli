ac77c7182d5b299629040655b354a3edb6acda05
FlareFlo <mail@flareflo.dev>
2022-04-14 15:05:05 +0200

    Adjusted use of flags

--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,26 @@ use semver::Version;
 #[derive(Parser, Debug)]
 #[clap(version)]
 struct Args {
-	/// The base version given
+	/// The base and target version seperated by `:`  to compare like `1.1.0:1.2.0`
 	#[clap(short, long)]
-	base: String,
-
-	/// The target version in relation the base
-	#[clap(short, long)]
-	target: String,
+	cmp: String,
 
 	/// long or short output, defaults to long
 	#[clap(short, long)]
-	long_output: bool,
+	verbose: bool,
 }
 
 fn main() {
 	let args = Args::parse();
 
-	let base = Version::parse(&args.base).unwrap();
-	let target = Version::parse(&args.target).unwrap();
+	let split = &args.cmp.split(':').collect::<Vec<&str>>();
+	let base = Version::parse(&split[0]).unwrap();
+	let target = Version::parse(&split[1]).unwrap();
 
-	if args.long_output {
-		let age = |cmp| return if cmp { "newer" } else { "older" };
+	if args.verbose {
+		let age = || return if base > target { "newer" } else { "older" };
 
-		println!("Base version {base} is {} than {target}", age(base > target));
+		println!("Base version {base} is {} than {target}", age());
 	} else {
 		println!("{}", base < target);
 	}
